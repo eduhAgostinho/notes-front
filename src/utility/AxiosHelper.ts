00e333ea3d93e427815c6fa570c29ed2f9004fdd
Eduardo Agostinho <eduardo.agostinho@spmassets.com>
2022-09-25 19:42:48 -0300

    added .env file

--- src/utility/AxiosHelper.ts
+++ src/utility/AxiosHelper.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const configureAxiosSettings = () => {
-    axios.defaults.baseURL = "http://localhost:4000";
+    const HOST = process.env.REACT_APP_API_HOST || 'localhost';
+    const PORT = process.env.REACT_APP_API_PORT || '3000';
+    axios.defaults.baseURL = `http://${HOST}:${PORT}`;
     axios.defaults.headers.post['Content-Type'] = 'application/json';
 }
